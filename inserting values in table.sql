c58a1ded0339990e14e1907d004762b76c42f2ce
meeta haldar <meetahaldar1001@gmail.com>
2021-06-16 18:07:08 +0530

    inserting values in table

---
+++ inserting values in table.sql
@@ -0,0 +1,9 @@
+INSERT INTO personal(
+
+id,name,branch,age,city,phone
+)
+VALUES
+(1, "john doe", "It" ,18, "" , 39772682),
+(2, "Alex jensen", "civil" ,19, "US" , 39972682),
+(3, "JUstin down", "Mechanical" ,18, "" , 25772682),
+(4, "peter parker", "It" ,21, "Canada " , 3977244443);
